7ff541e85aeb7840edfd8883543a21f915760c1b
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-09-09 14:54:31 -0300

    Feat: All safe notes functionalities implemented

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import authRouter from "./authRouter"
 import credentialRouter from "./credentialRouter"
+import safeNotesRouter from "./safeNotesRouter"
 
 const router = Router();
 
 router.use(authRouter);
 router.use(credentialRouter);
+router.use(safeNotesRouter);
 
 
 export default router;
